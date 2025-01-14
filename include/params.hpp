688f5bc000b7699aa6c3c664a2d4f243ff0105f0
otischung <p76124605@gs.ncku.edu.tw>
2025-01-14 18:43:52 +0800

    [FIX] Remove static

--- include/params.hpp
+++ include/params.hpp
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 #define UPDATE_ARM_DELAY 1.0
-static constexpr uint8_t NUM_OF_SERVOS = 6;
-static constexpr uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0};
-static constexpr uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 90};
-static constexpr uint8_t servoInitAngles[] = {10, 170, 80, 60, 80, 10};
+const size_t NUM_OF_SERVOS = 6;
+const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0};
+const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 90};
+const uint8_t servoInitAngles[] = {10, 170, 80, 60, 80, 10};
