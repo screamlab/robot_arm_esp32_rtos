eb4c517f3f4745d58ddecd1caa7271444fdbcc8c
otischung <p76124605@gs.ncku.edu.tw>
2025-01-15 14:39:14 +0800

    [REFACTOR] Split the instance of the armDriver header file to cpp file

--- include/armDriver.hpp
+++ include/armDriver.hpp
@@ -1,12 +1,36 @@
+#pragma once
 #include <Adafruit_PWMServoDriver.h>
+
+#include <params.hpp>
 
 /*
  * Use Adafruit_PWMServoDriver to control multiple servos.
  * Pin : use I2C , SDA 21 SCL 22
  *
  */
-const float ARM_MOVEMENT_STEP = 1.0;
 
+/**
+ * @class ArmManager
+ * @brief Manages the servos of a robotic arm using the Adafruit PWM Servo Driver.
+ *
+ * This class provides methods to initialize and control multiple servos connected to a
+ * robotic arm. It allows setting target angles for each servo and moves the servos
+ * incrementally towards their target angles.
+ *
+ * @details
+ * The ArmManager class handles the initialization of the Adafruit PWM Servo Driver,
+ * sets the PWM frequency, and manages the angles of the servos. It ensures that the
+ * servos move smoothly by incrementing their angles in small steps.
+ *
+ * @note
+ * The servo angles are constrained within the specified minimum and maximum angles
+ * to prevent damage to the servos or the robotic arm.
+ *
+ * @param numServos The number of servos to manage.
+ * @param servoMinAngles An array of minimum angles for each servo.
+ * @param servoMaxAngles An array of maximum angles for each servo.
+ * @param servoInitAngles An array of initial angles for each servo.
+ */
 class ArmManager {
    private:
     static const uint16_t SERVO_MIN_PULSE_WIDTH = 500;
@@ -20,113 +44,52 @@ class ArmManager {
     uint8_t *servoMinAngles;
     uint8_t *servoMaxAngles;
 
-    void setServoAngle(uint8_t servoNum, float angle) {
-        if (servoNum < this->numServos) {
-            pwm.writeMicroseconds(
-                servoNum,
-                map(
-                    angle, 0, 180,
-                    SERVO_MIN_PULSE_WIDTH, SERVO_MAX_PULSE_WIDTH
-                )
-            );
-        }
-    }
+    /**
+     * @brief Sets the angle of a specified servo.
+     *
+     * @param servoNum The index of the servo to set the angle for.
+     * @param angle The target angle to set for the servo.
+     */
+    void setServoAngle(uint8_t servoNum, float angle);
 
    public:
-    ArmManager(const uint8_t numServos, const uint8_t servoMinAngles[], const uint8_t servoMaxAngles[], const uint8_t servoInitAngles[]) {
-        // Initialize the Adafruit_PWMServoDriver object
-        this->numServos = numServos;
+    /**
+     * @brief Constructs an ArmManager object.
+     *
+     * @param numServos The number of servos to manage.
+     * @param servoMinAngles An array of minimum angles for each servo.
+     * @param servoMaxAngles An array of maximum angles for each servo.
+     * @param servoInitAngles An array of initial angles for each servo.
+     */
+    ArmManager(
+        const uint8_t numServos, const uint8_t servoMinAngles[],
+        const uint8_t servoMaxAngles[], const uint8_t servoInitAngles[]);
 
-        this->pwm.begin();
-        this->pwm.setOscillatorFrequency(27000000);
-        this->pwm.setPWMFreq(50);  // Set the PWM frequency to 50Hz
-        this->servoTargetAngles = new uint8_t[numServos];
-        this->servoCurrentAngles = new float[numServos];
-        this->servoMinAngles = new uint8_t[numServos];
-        this->servoMaxAngles = new uint8_t[numServos];
-        // Set servoMinAngles and servoMaxAngles
-        for (uint8_t i = 0; i < numServos; i++) {
-            this->servoMinAngles[i] = servoMinAngles[i];
-            this->servoMaxAngles[i] = servoMaxAngles[i];
-            this->servoTargetAngles[i] = servoInitAngles[i];
-            // this->servoTargetAngles[i] = 90;
-            /************************************************************************* 
-            You cannot set the current angles by reading the initial angles directly.
-            You can set the current angles to be "near" the initial angles
-                to ensure that the robot arm doesn't perform redundant actions.
-            If you set the current angles to be the same as the initial angles,
-                then the robot arm will not move when starting up.
-            **************************************************************************/
-            this->servoCurrentAngles[i] = (float)servoInitAngles[i] + 1;
-            // this->servoCurrentAngles[i] = 80;
-            Serial.printf("this->servoTargetAngles[%d] are ", i);
-            Serial.println(this->servoTargetAngles[i]);
-            Serial.printf("this->servoCurrentAngles[%d] are ", i);
-            Serial.println(this->servoCurrentAngles[i]);
-        }
-    }
+    /**
+     * @brief Sets the target angle for a specified servo.
+     *
+     * @param servoNum The index of the servo to set the target angle for.
+     * @param targetAngle The target angle to set for the servo.
+     */
+    void setServoTargetAngle(uint8_t servoNum, uint8_t targetAngle);
 
-    void setServoTargetAngle(uint8_t servoNum, uint8_t targetAngle) {
-        if (servoNum < this->numServos) {
-            this->servoTargetAngles[servoNum] = constrain(targetAngle, servoMinAngles[servoNum],
-                                                          servoMaxAngles[servoNum]);
-        }
-    }
+    /**
+     * @brief Changes the target angle of a specified servo by a bias angle.
+     *
+     * @param servoNum The index of the servo to change the target angle for.
+     * @param biasAngle The bias angle to add to the current target angle.
+     */
+    void changeServoTargetAngle(uint8_t servoNum, int8_t biasAngle);
 
-    void changeServoTargetAngle(uint8_t servoNum, int8_t biasAngle) {
-        if (servoNum < this->numServos) {
-            this->servoTargetAngles[servoNum] = constrain(servoTargetAngles[servoNum] + biasAngle,
-                                                          servoMinAngles[servoNum], servoMaxAngles[servoNum]);
-        }
-    }
+    /**
+     * @brief Gets the current angles of all servos.
+     *
+     * @param currentAngles An array to store the current angles of all servos.
+     */
+    void getCurrentAngles(float currentAngles[]);
 
-    // Get the current angles of all servos, update currentAngles into passed array
-    void getCurrentAngles(float currentAngles[]) {
-        for (uint8_t i = 0; i < this->numServos; i++) {
-            currentAngles[i] = servoCurrentAngles[i];
-        }
-    }
-
-    void moveArm() {
-        for (uint8_t i = 0; i < this->numServos; i++) {
-            if (abs(this->servoCurrentAngles[i] - this->servoTargetAngles[i]) >= ARM_MOVEMENT_STEP) {
-                float step = (this->servoTargetAngles[i] > this->servoCurrentAngles[i]) ? ARM_MOVEMENT_STEP
-                                                                                        : -ARM_MOVEMENT_STEP;
-                this->servoCurrentAngles[i] += step;
-                // this line will occur some delay to let device work not properly
-                // please make sure you had connect to PCA9685 pwm driver.
-                setServoAngle(i, this->servoCurrentAngles[i]);
-                // Serial.printf("this->servoTargetAngles[%d] are ",i);
-                // Serial.println(this->servoTargetAngles[i]);
-                // Serial.printf("this->servoCurrentAngles[%d] are ",i);
-                // Serial.println(this->servoCurrentAngles[i]);
-            }
-        }
-    }
-
-    void printStatus() {
-        Serial.println("Target:");
-        for (uint8_t i = 0; i < this->numServos; i++) {
-            Serial.print(this->servoTargetAngles[i]);
-            Serial.print(",");
-        }
-        Serial.println();
-
-        Serial.println("Current:");
-        for (uint8_t i = 0; i < this->numServos; i++) {
-            Serial.print(this->servoCurrentAngles[i]);
-            Serial.print(",");
-        }
-        Serial.println();
-    }
-
-    // void init()
-    // {
-    //     for (uint8_t i = 0; i < this->numServos; i++)
-    //     {
-    //         this->servoTargetAngles[i] = initAngles[i];
-    //         this->servoCurrentAngles[i] = initAngles[i];
-    //         this->setServoAngle(i, this->servoCurrentAngles[i]);
-    //     }
-    // }
+    /**
+     * @brief Moves the arm by incrementing the angles of the servos towards their target angles.
+     */
+    void moveArm();
 };
--- include/params.hpp
+++ include/params.hpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #define UPDATE_ARM_DELAY 1.0
+const float ARM_MOVEMENT_STEP = 1.0;
 const size_t NUM_OF_SERVOS = 6;
 const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0};
 const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 90};
