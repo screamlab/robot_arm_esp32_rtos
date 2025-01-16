612abcfe58aa93833b3b9f7337a39f6eec10d88f
otischung <p76124605@gs.ncku.edu.tw>
2025-01-16 17:50:55 +0800

    [FIX] Modify J4 init angle from 60 to 10

--- include/params.hpp
+++ include/params.hpp
@@ -6,4 +6,4 @@ const float ARM_MOVEMENT_STEP = 1.0;
 const size_t NUM_OF_SERVOS = 6;
 const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0};
 const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 90};
-const uint8_t servoInitAngles[] = {10, 170, 80, 60, 80, 10};
+const uint8_t servoInitAngles[] = {10, 170, 80, 10, 80, 10};
