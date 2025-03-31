ec67b08f5d5ac3250e3eaf48295738522b4edea0
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-03-31 11:33:51 +0800

    [FIX] Merge 2 sub to 1 to prevent race condition
    The original settings first subscribe to arm topic and hand topic, then
    republish to the corresponding topics. When the ESP32 keep receives data
    from the arm topic, there may have some data loss from the hand topic.
    We've tried the followings, but doesn't work properly:
    1. Use 1 executor for 2 subs and 2 pubs.
    2. Use multithreading and enable UCLIENT_PROFILE_MULTITHREAD=ON
    3. Decrease the timeout of each sub and pub to increase the refresh
       rate.
    
    After these tries, we decided to merge 2 subscriptions into 1 and merge
    these 2 topics outside the Micro ROS.

--- include/params.hpp
+++ include/params.hpp
@@ -18,10 +18,8 @@
 #define ROS_DOMAIN_ID 0
 #define NODE_NAME "micro_ros_platformio_left_node"
 #define NAMESPACE ""
-#define ARM_TOPIC_NAME "/left_arm"
-#define HAND_TOPIC_NAME "/left_hand"
-#define ARM_REPUBLISH_TOPIC_NAME "/left_arm_republish"
-#define HAND_REPUBLISH_TOPIC_NAME "/left_hand_republish"
+#define TOPIC_NAME "/left"
+#define REPUBLISH_TOPIC_NAME "/left_republish"
 const uint8_t servoMinAngles[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t servoMaxAngles[] = {180, 90, 180, 120, 180, 180, 180, 180, 180, 180, 180};
 const uint8_t servoInitAngles[] = {170, 10, 100, 10, 100, 100, 180, 0, 0, 0, 0};
@@ -29,10 +27,8 @@ const uint8_t servoInitAngles[] = {170, 10, 100, 10, 100, 100, 180, 0, 0, 0, 0};
 #define ROS_DOMAIN_ID 0
 #define NODE_NAME "micro_ros_platformio_right_node"
 #define NAMESPACE ""
-#define ARM_TOPIC_NAME "/right_arm"
-#define HAND_TOPIC_NAME "/right_hand"
-#define ARM_REPUBLISH_TOPIC_NAME "/right_arm_republish"
-#define HAND_REPUBLISH_TOPIC_NAME "/right_hand_republish"
+#define TOPIC_NAME "/right"
+#define REPUBLISH_TOPIC_NAME "/right_republish"
 const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 180, 180, 180, 180, 180, 180};
 const uint8_t servoInitAngles[] = {10, 170, 80, 10, 80, 80, 0, 180, 180, 180, 180};
@@ -41,16 +37,11 @@ const uint8_t servoInitAngles[] = {10, 170, 80, 10, 80, 80, 0, 180, 180, 180, 18
 #define ESP32_LED 2
 #define UPDATE_ARM_DELAY 1.0
 const float ARM_MOVEMENT_STEP = 1.0;
-const size_t NUM_ALL_SERVOS = 11;
-const size_t NUM_ARM_SERVOS = 6;
-const size_t NUM_HAND_SERVOS = 5;
-const size_t ARM_OFFSET = 0;
-const size_t HAND_OFFSET = NUM_ARM_SERVOS;
+const size_t NUM_SERVOS = 11;
 
-static_assert(sizeof(servoMinAngles) == NUM_ALL_SERVOS * sizeof(uint8_t));
-static_assert(sizeof(servoMaxAngles) == NUM_ALL_SERVOS * sizeof(uint8_t));
-static_assert(sizeof(servoInitAngles) == NUM_ALL_SERVOS * sizeof(uint8_t));
-static_assert(NUM_ARM_SERVOS + NUM_HAND_SERVOS == NUM_ALL_SERVOS);
+static_assert(sizeof(servoMinAngles) == NUM_SERVOS * sizeof(uint8_t));
+static_assert(sizeof(servoMaxAngles) == NUM_SERVOS * sizeof(uint8_t));
+static_assert(sizeof(servoInitAngles) == NUM_SERVOS * sizeof(uint8_t));
 
 enum states {
     WAITING_AGENT,
--- src/main.cpp
+++ src/main.cpp
@@ -15,51 +15,34 @@ rclc_support_t support;
 rcl_node_t node;
 
 // subscriber
-rcl_subscription_t arm_sub, hand_sub;
-trajectory_msgs__msg__JointTrajectoryPoint arm_msg_sub, hand_msg_sub;
-rclc_executor_t arm_executor_sub, hand_executor_sub;
+rcl_subscription_t sub;
+trajectory_msgs__msg__JointTrajectoryPoint msg_sub;
+rclc_executor_t executor_sub;
 
 // publisher
-rcl_publisher_t arm_pub, hand_pub;
-trajectory_msgs__msg__JointTrajectoryPoint arm_msg_pub, hand_msg_pub;
-rclc_executor_t arm_executor_pub, hand_executor_pub;
-rcl_timer_t arm_timer, hand_timer;
+rcl_publisher_t pub;
+trajectory_msgs__msg__JointTrajectoryPoint msg_pub;
+rclc_executor_t executor_pub;
+rcl_timer_t timer;
 
 // Global variables shared between the microROS task and the arm control task
-double joint_positions[NUM_ALL_SERVOS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+double joint_positions[NUM_SERVOS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 states state;
 
-void arm_subscription_callback(const void *msgin) {
+void subscription_callback(const void *msgin) {
     const trajectory_msgs__msg__JointTrajectoryPoint *msg = (const trajectory_msgs__msg__JointTrajectoryPoint *)msgin;
     for (size_t i = 0; i < msg->positions.size; ++i) {
-        joint_positions[i + ARM_OFFSET] = degrees(msg->positions.data[i]);
+        joint_positions[i] = degrees(msg->positions.data[i]);
     }
 }
 
-void arm_timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
     RCLC_UNUSED(last_call_time);
     if (timer != NULL) {
-        RCSOFTCHECK(rcl_publish(&arm_pub, &arm_msg_pub, NULL));
-        for (size_t i = 0; i < arm_msg_pub.positions.capacity; i++) {
-            arm_msg_pub.positions.data[i] = joint_positions[i + ARM_OFFSET];
-        }
-    }
-}
-
-void hand_subscription_callback(const void *msgin) {
-    const trajectory_msgs__msg__JointTrajectoryPoint *msg = (const trajectory_msgs__msg__JointTrajectoryPoint *)msgin;
-    for (size_t i = 0; i < msg->positions.size; ++i) {
-        joint_positions[i + HAND_OFFSET] = degrees(msg->positions.data[i]);
-    }
-}
-
-void hand_timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
-    RCLC_UNUSED(last_call_time);
-    if (timer != NULL) {
-        RCSOFTCHECK(rcl_publish(&hand_pub, &hand_msg_pub, NULL));
-        for (size_t i = 0; i < hand_msg_pub.positions.capacity; i++) {
-            hand_msg_pub.positions.data[i] = joint_positions[i + HAND_OFFSET];
+        RCSOFTCHECK(rcl_publish(&pub, &msg_pub, NULL));
+        for (size_t i = 0; i < msg_pub.positions.capacity; i++) {
+            msg_pub.positions.data[i] = joint_positions[i];
         }
     }
 }
@@ -75,17 +58,11 @@ bool create_entities() {
     // create node
     FNCHECK(create_node(&allocator, &init_options, &support, &node), bool);
 
-    // create arm subscriber
-    FNCHECK(create_subscriber(&allocator, &support, &node, ARM_TOPIC_NAME, &arm_sub, arm_subscription_callback, &arm_msg_sub, NUM_ARM_SERVOS, &arm_executor_sub), bool);
+    // create subscriber
+    FNCHECK(create_subscriber(&allocator, &support, &node, TOPIC_NAME, &sub, subscription_callback, &msg_sub, NUM_SERVOS, &executor_sub), bool);
 
-    // create hand subscriber
-    FNCHECK(create_subscriber(&allocator, &support, &node, HAND_TOPIC_NAME, &hand_sub, hand_subscription_callback, &hand_msg_sub, NUM_HAND_SERVOS, &hand_executor_sub), bool);
-
-    // create arm publisher
-    FNCHECK(create_publisher(&allocator, &support, &node, ARM_REPUBLISH_TOPIC_NAME, &arm_pub, &arm_timer, arm_timer_callback, &arm_msg_pub, NUM_ARM_SERVOS, &arm_executor_pub), bool);
-
-    // create hand publisher
-    FNCHECK(create_publisher(&allocator, &support, &node, HAND_REPUBLISH_TOPIC_NAME, &hand_pub, &hand_timer, hand_timer_callback, &hand_msg_pub, NUM_HAND_SERVOS, &hand_executor_pub), bool);
+    // create publisher
+    FNCHECK(create_publisher(&allocator, &support, &node, REPUBLISH_TOPIC_NAME, &pub, &timer, timer_callback, &msg_pub, NUM_SERVOS, &executor_pub), bool);
 
     return true;
 }
@@ -94,17 +71,11 @@ void destroy_entities() {
     rmw_context_t *rmw_context = rcl_context_get_rmw_context(&support.context);
     (void)rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
 
-    // arm subscriber
-    delete_subscriber(&node, &arm_sub, &arm_msg_sub, &arm_executor_sub);
+    // subscriber
+    delete_subscriber(&node, &sub, &msg_sub, &executor_sub);
 
-    // hand subscriber
-    delete_subscriber(&node, &hand_sub, &hand_msg_sub, &hand_executor_sub);
-
-    // arm publisher
-    delete_publisher(&node, &arm_pub, &arm_timer, &arm_msg_pub, &arm_executor_pub);
-
-    // hand publisher
-    delete_publisher(&node, &hand_pub, &hand_timer, &hand_msg_pub, &hand_executor_pub);
+    // publisher
+    delete_publisher(&node, &pub, &timer, &msg_pub, &executor_pub);
 
     // common
     delete_node(&init_options, &support, &node);
@@ -122,15 +93,13 @@ void microROSTaskFunction(void *parameter) {
                 state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
                 if (state == WAITING_AGENT) {
                     destroy_entities();
-                };
+                }
                 break;
             case AGENT_CONNECTED:
                 EXECUTE_EVERY_N_MS(2000, state = (RMW_RET_OK == rmw_uros_ping_agent(20, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
                 if (state == AGENT_CONNECTED) {
-                    rclc_executor_spin_some(&arm_executor_sub, RCL_MS_TO_NS(100));
-                    rclc_executor_spin_some(&hand_executor_sub, RCL_MS_TO_NS(100));
-                    rclc_executor_spin_some(&arm_executor_pub, RCL_MS_TO_NS(100));
-                    rclc_executor_spin_some(&hand_executor_pub, RCL_MS_TO_NS(100));
+                    rclc_executor_spin_some(&executor_sub, RCL_MS_TO_NS(50));
+                    rclc_executor_spin_some(&executor_pub, RCL_MS_TO_NS(50));
                 }
                 break;
             case AGENT_DISCONNECTED:
@@ -144,10 +113,10 @@ void microROSTaskFunction(void *parameter) {
 }
 
 void armControlTaskFunction(void *parameter) {
-    ArmManager armManager(uint8_t(NUM_ALL_SERVOS), servoMinAngles, servoMaxAngles, servoInitAngles);
+    ArmManager armManager(uint8_t(NUM_SERVOS), servoMinAngles, servoMaxAngles, servoInitAngles);
 
     while (true) {
-        for (size_t i = 0; i < NUM_ALL_SERVOS; ++i) {
+        for (size_t i = 0; i < NUM_SERVOS; ++i) {
             armManager.setServoTargetAngle(i, uint8_t(joint_positions[i]));
         }
         armManager.moveArm();
@@ -166,7 +135,7 @@ void setup() {
     state = WAITING_AGENT;
 
     // Initialize joint_positions with the initial angles
-    for (size_t i = 0; i < NUM_ALL_SERVOS; ++i) {
+    for (size_t i = 0; i < NUM_SERVOS; ++i) {
         joint_positions[i] = double(servoInitAngles[i]);
     }
 
