6e98da7d0a85ee76ef93b65f77b0d6ec08f5f4ad
Otis Chung <p76124605@gs.ncku.edu.tw>
2025-04-03 16:27:34 +0800

    [FIX] Adjust hand joints' limit

--- include/params.hpp
+++ include/params.hpp
@@ -20,18 +20,18 @@
 #define NAMESPACE ""
 #define TOPIC_NAME "/left"
 #define REPUBLISH_TOPIC_NAME "/left_republish"
-const uint8_t servoMinAngles[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-const uint8_t servoMaxAngles[] = {180, 90, 180, 120, 180, 180, 180, 180, 180, 180, 180};
-const uint8_t servoInitAngles[] = {170, 10, 100, 10, 100, 100, 180, 0, 0, 0, 0};
+const uint8_t servoMinAngles[] = {0, 0, 0, 0, 0, 0, 20, 50, 50, 50, 50};
+const uint8_t servoMaxAngles[] = {180, 90, 180, 120, 180, 180, 130, 155, 155, 155, 155};
+const uint8_t servoInitAngles[] = {170, 10, 100, 10, 100, 100, 130, 50, 50, 50, 50};
 #else
 #define ROS_DOMAIN_ID 0
 #define NODE_NAME "micro_ros_platformio_right_node"
 #define NAMESPACE ""
 #define TOPIC_NAME "/right"
 #define REPUBLISH_TOPIC_NAME "/right_republish"
-const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 180, 180, 180, 180, 180, 180};
-const uint8_t servoInitAngles[] = {10, 170, 80, 10, 80, 80, 0, 180, 180, 180, 180};
+const uint8_t servoMinAngles[] = {0, 80, 0, 0, 0, 0, 60, 15, 15, 15, 15};
+const uint8_t servoMaxAngles[] = {180, 180, 180, 120, 180, 180, 165, 120, 120, 120, 120};
+const uint8_t servoInitAngles[] = {10, 170, 80, 10, 80, 80, 60, 120, 120, 120, 120};
 #endif
 
 #define ESP32_LED 2
