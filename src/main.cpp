6d2cfe3fa52549335f0dfdfe541009981dc10ed8
otischung <p76124605@gs.ncku.edu.tw>
2025-08-07 10:00:23 +0800

    [REFACTOR] Refactor all code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,50 @@
 #include <Arduino.h>
+#include <micro_ros_platformio.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <trajectory_msgs/msg/joint_trajectory_point.h>
 
 #include <armDriver.hpp>
 #include <esp32_led.hpp>
-#include <micro_ros_utils.hpp>
 #include <params.hpp>
 
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 #error This example is only avaliable for Arduino framework with serial transport.
 #endif
 
+#define RCCHECK(fn)                    \
+    {                                  \
+        rcl_ret_t temp_rc = fn;        \
+        if ((temp_rc != RCL_RET_OK)) { \
+            return false;              \
+        }                              \
+    }
+#define RCSOFTCHECK(fn)                \
+    {                                  \
+        rcl_ret_t temp_rc = fn;        \
+        if ((temp_rc != RCL_RET_OK)) { \
+        }                              \
+    }
+#define FNCHECK(fn, type_) \
+    {                      \
+        type_ ret = fn;    \
+        if (!ret) {        \
+            return false;  \
+        }                  \
+    }
+#define EXECUTE_EVERY_N_MS(MS, X)          \
+    do {                                   \
+        static volatile int64_t init = -1; \
+        if (init == -1) {                  \
+            init = uxr_millis();           \
+        }                                  \
+        if (uxr_millis() - init > MS) {    \
+            X;                             \
+            init = uxr_millis();           \
+        }                                  \
+    } while (0)
+
+/* Micro-ROS specific initialization */
 rcl_allocator_t allocator;
 rcl_init_options_t init_options;
 rclc_support_t support;
@@ -17,25 +53,30 @@ rcl_node_t node;
 // subscriber
 rcl_subscription_t sub;
 trajectory_msgs__msg__JointTrajectoryPoint msg_sub;
-rclc_executor_t executor_sub;
 
 #ifdef USE_REPUBLISH
 // publisher
 rcl_publisher_t pub;
 trajectory_msgs__msg__JointTrajectoryPoint msg_pub;
-rclc_executor_t executor_pub;
 rcl_timer_t timer;
+const uint64_t timer_timeout = RCL_MS_TO_NS(100);
 #endif
+
+// executor
+rclc_executor_t executor;
+size_t num_handles = 1;
 
 // Global mutex declaration
 #ifdef USE_MUTEX_LOCK
 SemaphoreHandle_t xJointPositionsMutex = NULL;
 #endif
 
-// Global variables shared between the microROS task and the arm control task
+// Global variables shared between the Micro-ROS task and the arm-control task
 double joint_positions[NUM_SERVOS] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+// Counter for the number of messages received
 uint64_t msg_cnt = 0;
 
+// State machine for the agent connection
 states state;
 
 void subscription_callback(const void *msgin) {
@@ -92,15 +133,67 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
  */
 
 bool create_entities() {
-    // create node
-    FNCHECK(create_node(&allocator, &init_options, &support, &node), bool);
+    // Create node
+    allocator = rcl_get_default_allocator();
+    init_options = rcl_get_zero_initialized_init_options();
+    RCCHECK(rcl_init_options_init(&init_options, allocator));
+    RCCHECK(rcl_init_options_set_domain_id(&init_options, ROS_DOMAIN_ID));
 
-    // create subscriber
-    FNCHECK(create_subscriber(&allocator, &support, &node, TOPIC_NAME, &sub, subscription_callback, &msg_sub, NUM_SERVOS, &executor_sub), bool);
+    RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+    RCCHECK(rclc_node_init_default(&node, NODE_NAME, NAMESPACE, &support));
+
+    // Create subscriber
+    RCCHECK(rclc_subscription_init(
+        &sub,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(trajectory_msgs, msg, JointTrajectoryPoint),
+        TOPIC_NAME,
+        &rmw_qos_profile_sensor_data));
+
+    msg_sub.positions.capacity = NUM_SERVOS;
+    msg_sub.positions.size = NUM_SERVOS;
+    msg_sub.positions.data = (double *)calloc(msg_sub.positions.capacity, sizeof(double));
+    if (!msg_sub.positions.data)
+        return false;
 
 #ifdef USE_REPUBLISH
-    // create publisher
-    FNCHECK(create_publisher(&allocator, &support, &node, REPUBLISH_TOPIC_NAME, &pub, &timer, timer_callback, &msg_pub, NUM_SERVOS, &executor_pub), bool);
+    // Create publisher
+    RCCHECK(rclc_publisher_init(
+        &pub,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(trajectory_msgs, msg, JointTrajectoryPoint),
+        REPUBLISH_TOPIC_NAME,
+        &rmw_qos_profile_sensor_data));
+
+    // Create timer
+    RCCHECK(rclc_timer_init_default(
+        &timer,
+        &support,
+        timer_timeout,
+        timer_callback));
+
+    msg_pub.positions.capacity = NUM_SERVOS;
+    msg_pub.positions.size = NUM_SERVOS;
+    msg_pub.positions.data = (double *)calloc(msg_pub.positions.capacity, sizeof(double));
+    if (!msg_pub.positions.data)
+        return false;
+#endif
+
+        // Create executor
+#ifdef USE_REPUBLISH
+    // Since we republish data for each topic,
+    // we need to double the number of handles.
+    RCCHECK(rclc_executor_init(&executor, &support.context, num_handles << 1, &allocator));
+#else
+    RCCHECK(rclc_executor_init(&executor, &support.context, num_handles, &allocator));
+#endif
+
+    // Add the subscriber to the executor
+    RCCHECK(rclc_executor_add_subscription(&executor, &sub, &msg_sub, subscription_callback, ON_NEW_DATA));
+
+#ifdef USE_REPUBLISH
+    // Add the publisher to the executor
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
 #endif
 
     return true;
@@ -111,15 +204,24 @@ void destroy_entities() {
     (void)rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
 
     // subscriber
-    delete_subscriber(&node, &sub, &msg_sub, &executor_sub);
+    RCSOFTCHECK(rcl_subscription_fini(&sub, &node));
+    RCSOFTCHECK(rclc_executor_fini(&executor));
+    trajectory_msgs__msg__JointTrajectoryPoint__fini(&msg_sub);
 
 #ifdef USE_REPUBLISH
     // publisher
-    delete_publisher(&node, &pub, &timer, &msg_pub, &executor_pub);
+    RCSOFTCHECK(rcl_publisher_fini(&pub, &node));
+    RCSOFTCHECK(rcl_timer_fini(&timer));
+    trajectory_msgs__msg__JointTrajectoryPoint__fini(&msg_pub);
 #endif
 
+    // executor
+    RCSOFTCHECK(rclc_executor_fini(&executor));
+
     // common
-    delete_node(&init_options, &support, &node);
+    RCSOFTCHECK(rcl_node_fini(&node));
+    RCSOFTCHECK(rclc_support_fini(&support));
+    RCSOFTCHECK(rcl_init_options_fini(&init_options));
 }
 
 void microROSTaskFunction(void *parameter) {
@@ -139,10 +241,7 @@ void microROSTaskFunction(void *parameter) {
             case AGENT_CONNECTED:
                 EXECUTE_EVERY_N_MS(2000, state = (RMW_RET_OK == rmw_uros_ping_agent(200, 2)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
                 if (state == AGENT_CONNECTED) {
-                    rclc_executor_spin_some(&executor_sub, RCL_MS_TO_NS(50));
-#ifdef USE_REPUBLISH
-                    rclc_executor_spin_some(&executor_pub, RCL_MS_TO_NS(50));
-#endif
+                    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
                 }
                 break;
             case AGENT_DISCONNECTED:
